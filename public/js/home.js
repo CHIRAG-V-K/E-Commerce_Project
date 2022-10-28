4362040559617095ec8ecaa9dc44c32409bb2b6f
CHIRAG-V-K <chirag.v.k2@gmail.com>
2022-10-28 11:33:58 +0530

    initials

--- public/js/home.js
+++ public/js/home.js
@@ -1,3 +1,14 @@
+// to add image data
+
+// function chirag(){
+//     let data = [{"iid":"1","iname":"lap1.jpg","amount":"30000","desc":"HP Pro-Book","quantity":"1"},{"iid":"2","iname":"lap2.jpg","amount":"40000","desc":"Chrome-Book","quantity":"1"},{"iid":"3","iname":"lap3.jpg","amount":"35000","desc":"Lenovo Legion","quantity":"1"},{"iid":"4","iname":"lap4.jpg","amount":"100000","desc":"Mac-Book Pro","quantity":"1"},{"iid":"5","iname":"lap5.jpg","amount":"50000","desc":"Hp Pavillion","quantity":"1"},{"iid":"6","iname":"lap6.jpg","amount":"55000","desc":"Asus TUF Gaming","quantity":"1"},{"iid":"7","iname":"lap7.jpg","amount":"50000","desc":"Mi Note-Book","quantity":"1"},{"iid":"8","iname":"lap8.jpg","amount":"43000","desc":"Dell H5483-21G","quantity":"1"}];
+    
+//     localStorage.setItem("Collections",JSON.stringify(data))
+
+// }
+
+
+
 function registerUser(){
     let Name = document.getElementById('name').value;
     let Phone = document.getElementById('phone').value;
@@ -99,14 +110,6 @@ function login(){
 }
 
 
-// to add image data
-
-// function chirag(){
-//     let data = [{"iid":"1","iname":"lap1.jpg","amount":"30000","desc":"HP Pro-Book","quantity":"1"},{"iid":"2","iname":"lap2.jpg","amount":"40000","desc":"Chrome-Book","quantity":"1"},{"iid":"3","iname":"lap3.jpg","amount":"35000","desc":"Lenovo Legion","quantity":"1"},{"iid":"4","iname":"lap4.jpg","amount":"100000","desc":"Mac-Book Pro","quantity":"1"},{"iid":"5","iname":"lap5.jpg","amount":"50000","desc":"Hp Pavillion","quantity":"1"},{"iid":"6","iname":"lap6.jpg","amount":"55000","desc":"Asus TUF Gaming","quantity":"1"},{"iid":"7","iname":"lap7.jpg","amount":"50000","desc":"Mi Note-Book","quantity":"1"},{"iid":"8","iname":"lap8.jpg","amount":"43000","desc":"Dell H5483-21G","quantity":"1"}];
-    
-//     localStorage.setItem("Collections",JSON.stringify(data))
-
-// }
 
 
 function home(){
